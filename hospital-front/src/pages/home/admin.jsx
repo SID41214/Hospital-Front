97d74f774940faeaa6f0f69b778f74c8df138e1e
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-05-10 18:16:05 +0530

    updated

--- hospital-front/src/pages/home/admin.jsx
+++ hospital-front/src/pages/home/admin.jsx
@@ -3,8 +3,28 @@ import React from 'react'
 const admin = () => {
   return (
     <div>
+        <div>
+        
+        <div className="sidebar">
+
+        </div>
+
+        <div className="right">
+            <div className="doctor"></div>
+            <div className="users"></div>
+            <div className="appointment"></div>
+            <div>
+                <div className="below">
+                    <div className="bl"></div>
+                    <div className="br"></div>
+
+                </div>
+            </div>
+        </div>
 
 
+        
+        </div>
 
 
 
