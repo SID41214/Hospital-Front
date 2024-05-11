da8de53fdb97c1d43fcd1322a4f8a603fe7de9e1
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-05-11 12:27:07 +0530

    updated

--- hospital-front/src/App.js
+++ hospital-front/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-// import Home from './pages/home/home';
+import Home from './pages/home/home';
 import Admin from './pages/home/admin';
 
 function App() {
--- hospital-front/src/pages/home/admin.jsx
+++ hospital-front/src/pages/home/admin.jsx
@@ -5,18 +5,28 @@ const admin = () => {
     <div>
         <div>
         
-        <div className="sidebar">
-
+        <div className="sidebar  ">
+    <h2 className='bg-slate-950  text-white'>Side bar</h2>
         </div>
 
         <div className="right">
-            <div className="doctor"></div>
-            <div className="users"></div>
-            <div className="appointment"></div>
+            <div className="doctor">
+            <h2 className='bg-slate-600  text-white'> Doctor bar</h2>
+            </div>
+            <div className="users">
+            <h2 className='bg-red-800  text-white'>Users bar</h2>
+            </div>
+            <div className="appointment">
+            <h2 className='bg-yellow-400  text-white'>Appointment bar</h2>
+            </div>
             <div>
                 <div className="below">
-                    <div className="bl"></div>
-                    <div className="br"></div>
+                    <div className="bl">
+                    <h2 className='bg-lime-700  text-white'>bl bar</h2>
+                    </div>
+                    <div className="br">
+                    <h2 className='bg-blue-800 text-white'>br bar</h2>
+                    </div>
 
                 </div>
             </div>
